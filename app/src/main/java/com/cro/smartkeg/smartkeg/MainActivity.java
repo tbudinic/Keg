1a075d3e7c386d9dc5f909dbd09ba14e001883e7
tbudinic <35468563+tbudinic@users.noreply.github.com>
2018-04-07 23:30:13 -0400

    Hey Tony ;)))))

--- app/src/main/java/com/cro/smartkeg/smartkeg/MainActivity.java
+++ app/src/main/java/com/cro/smartkeg/smartkeg/MainActivity.java
@@ -68,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //I bet Tony will never see this!  TODOTODOTODOTODOTODOTODO
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
