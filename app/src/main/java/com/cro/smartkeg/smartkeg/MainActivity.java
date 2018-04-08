646cad47b4af36196bc28a890dc711fc2d4df69b
Eric Raguzin <eric.raguzin@gmail.com>
2018-04-08 00:09:33 -0400

    Conflict

--- app/src/main/java/com/cro/smartkeg/smartkeg/MainActivity.java
+++ app/src/main/java/com/cro/smartkeg/smartkeg/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //See I took out all the dumb comments
+        //Haha tony i will conflict you!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("Permission Bluetooth", ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)+"");
